361b67d6fbf7719c1fd52adf1ca4cf79e111167a
Stanley Sha <ssha2@uci.edu>
2025-03-14 22:15:28 +0000

    changing wl verif

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -144,17 +144,9 @@ void print_word_ladder(const vector<string>& ladder) {
     cout << endl;
 }
 
-// A simple verification routine that runs a few known tests
-#define my_assert(e) {cout << #e << ((e) ? " passed" : " failed") << endl;}
-
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
 
-    my_assert(generate_word_ladder("cat",   "dog",   word_list).size() == 4);
-    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-    my_assert(generate_word_ladder("code",  "data",  word_list).size() == 6);
-    my_assert(generate_word_ladder("work",  "play",  word_list).size() == 6);
-    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-    my_assert(generate_word_ladder("car",   "cheat", word_list).size() == 4);
+    vector<string> generate_word_ladder("cat",   "dog",   word_list);
 }
